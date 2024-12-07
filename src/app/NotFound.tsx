b9fac9539a5b08151f995a899cb293cf44090dc5
Myriam <myriamalbustany@gmail.com>
2024-12-08 01:10:39 +0200

    fixed error page ui to be consistent with the rest of the pages

--- src/app/NotFound.tsx
+++ src/app/NotFound.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-        <div>
-            <p>404 Not Found!</p>
-            <Link to="/">Home</Link>
+        <div className="flex items-center justify-center h-dvh w-dvw bg-midnight text-zinc">
+            <p className="p-2">
+                404 Not Found!
+            </p>
+            <Link to="/" className="p-2 hover:text-darkblue">
+                Return home?
+            </Link>
         </div>
     )
 }
