c33f0aaa8f955ed2a02b8a72320036440aab9288
Myriam <myriamalbustany@gmail.com>
2024-12-09 16:28:04 +0200

    added terminal viewports animations in overview page

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -25,24 +25,24 @@ function Header() {
 
        */
         <div className="h-dvh w-dvw bg-gradient-to-r from-color1 via-color2 to-color3 pt-20 pl-20 pr-20">
-            <div className="items-center bg-black bg-opacity-75 shadow-zinc shadow-xl pb-28">
+            <div className="items-center bg-black bg-opacity-75 shadow-zinc shadow-xl pb-24">
                 <div className="flex flex-row-reverse text-zinc">
-                    <p className="p-2">
+                    <p className="p-2 hover:text-darkblue">
                         <Link to="overview">Overview</Link>
                     </p>
-                    <p className="p-2">Work Experience</p>
-                    <p className="p-2">Tech Stack</p>
-                    <p className="p-2">Contact</p>
+                    <p className="p-2 hover:text-darkblue">Experience</p>
+                    <p className="p-2 hover:text-darkblue">Stack</p>
+                    <p className="p-2 hover:text-darkblue">Contact</p>
                 </div>
                 <div className="text-zinc pt-32 pb-32">
                     <div className="flex items-center justify-center p-2">
                         Hey there, I'm
                     </div>
                     <div className="flex items-center justify-center text-5xl font-bold">
-                        MYRIAM BOUSTANY
+                        myrmlbst
                     </div>
-                    <div className="flex items-center justify-center pt-2">
-                        Software engineer crafting business solutions through scripts and code
+                    <div className="flex items-center justify-center text-center pt-4">
+                        software engineer crafting business solutions through scripts and code
                     </div>
                 </div>
 
