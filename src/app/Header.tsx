b6c3b3f27e91f790ac9d14b2ada1f7e536c101d3
Myriam <myriamalbustany@gmail.com>
2024-12-14 18:33:08 +0200

    removed bloat

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,31 +2,9 @@ import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        /*
-        flex items-center justify-center
-        */
-
-        /*
-        <div className="h-dvh w-dvw bg-gradient-to-r from-color1 via-color2 to-color3">
-            <div className="flex items-center justify-center">
-                <div className="bg-black text-zinc mt-20 pl-10 pr-10">
-                    <div className="border-0">
-                        link1, link2, link3
-                    </div>
-                    <p className="text-2xl font-bold">
-                        myrmlbstn
-                    </p>
-                    <p>
-                        Crafting business solutions with software tools
-                    </p>
-                </div>
-            </div>
-        </div>
-
-       */
         <div className="h-dvh w-dvw bg-gradient-to-r from-color1 via-color2 to-color3 pt-20 pl-20 pr-20">
             <div className="items-center bg-black bg-opacity-75 shadow-zinc shadow-xl pb-24">
-                <div className="flex flex-row-reverse text-zinc">
+                <div className="border-2 border-yellow flex flex-row-reverse text-zinc">
                     <p className="p-2 hover:text-darkblue">
                         <Link to="overview">Overview</Link>
                     </p>
